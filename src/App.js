e4ca9362071343ec879f8e393eea89387053e9f7
ZiedHf <ziedhaffoudhi@gmail.com>
2017-12-04 22:00:10 +0100

    add itemsContainerComponent

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import React, { Component } from "react";
-import { ItemComponent } from "./components/ItemComponent";
-import { feedObject as feed } from "./assets/fakeApi";
+import ItemsContainerComponent from "./components/ItemsContainerComponent";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        {
-          feed.map((item, key) => (
-            <ItemComponent
-              key={key}
-              title={item.title}
-              userID={item.userID}
-              itemType={item.itemType}
-              category={item.category}
-              tag={item.tag}
-              country={item.country}
-              region={item.region}
-              price={item.price}
-              img={item.img}
-              short_desc={item.short_desc}
-              active={item.active}
-              ordred={item.ordred}
-              inFavorite={item.inFavorite} />
-          ))
-        }
-      </div>
+      <ItemsContainerComponent />
     );
   }
 }
---
+++ src/components/ItemsContainerComponent.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import ItemComponent from "./ItemComponent";
+import { feedObject as feed } from "../assets/fakeApi";
+
+class ItemsContainerComponent extends Component {
+  render() {
+    return (
+      <div>
+        {
+          feed.map((item, key) => (
+            <ItemComponent
+              key={key}
+              title={item.title}
+              userID={item.userID}
+              itemType={item.itemType}
+              category={item.category}
+              tag={item.tag}
+              country={item.country}
+              region={item.region}
+              price={item.price}
+              img={item.img}
+              short_desc={item.short_desc}
+              active={item.active}
+              ordred={item.ordred}
+              inFavorite={item.inFavorite} />
+          ))
+        }
+      </div>
+    );
+  }
+}
+
+export default ItemsContainerComponent;
